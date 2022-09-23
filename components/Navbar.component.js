34e9973b509c8445ffd24a405304111021503d1b
Bertil Tandayamo <bertilt4@gmail.com>
2022-09-23 12:34:21 -0500

    add navbar component module

---
+++ components/Navbar.component.js
@@ -0,0 +1,6 @@
+import styles from './Navbar.module.scss';
+const NavbarComponent = () => {
+  return <div>This is navbar</div>;
+};
+
+export default NavbarComponent;
--- layouts/HomePage.content.js
+++ layouts/HomePage.content.js
@@ -1,17 +1,18 @@
 import BodyComponent from '../components/Body.component';
 import FooterComponent from '../components/Footer.component';
 import HeadComponent from '../components/Head.component';
+import NavbarComponent from '../components/Navbar.component';
 import ScriptComponent from '../components/Script.component';
 
 function HomePage() {
   return (
     <div>
       <HeadComponent />
+      <NavbarComponent />
       <BodyComponent />
-      <FooterComponent/>
+      <FooterComponent />
 
       <ScriptComponent />
-      
     </div>
   );
 }
