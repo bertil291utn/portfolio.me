637d289091a055a203f3ba6da04eabeee7211d30
Bertil Tandayamo <bertilt4@gmail.com>
2022-09-27 14:08:01 -0500

    add active class

--- components/Navbar.component.js
+++ components/Navbar.component.js
@@ -2,16 +2,12 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './Navbar.module.scss';
 const NavbarComponent = ({ navbarElements }) => {
+  const router = useRouter();
   return (
     <ul className={styles['navbar']}>
       {Object.entries(navbarElements)?.map(([navbarName, pathName]) => (
         <li
-          className={
-            router.pathname == pathName
-              ? // || router.pathname == '/'
-                `${styles['active']}`
-              : ''
-          }
+          className={router.pathname == pathName ? `${styles['active']}` : ''}
         >
           <Link href={pathName}>{navbarName}</Link>
         </li>
