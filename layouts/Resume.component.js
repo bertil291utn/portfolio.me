bd4dd67d88ec63c9a81b98ae3690b0f0de13105b
Bertil Tandayamo <bertilt4@gmail.com>
2022-09-28 18:30:49 -0500

    - update resume json file - get resume from json data

--- layouts/Resume.component.js
+++ layouts/Resume.component.js
@@ -4,168 +4,99 @@ import ToggleComponent from '../components/common/Toggle.component';
 import resumeFile from '../data/resume.json';
 
 const ResumeComponent = () => {
+  const {
+    name,
+    title,
+    isAvailable,
+    description,
+    email,
+    location,
+    socialNetwork,
+    workExperience,
+    education,
+    skills,
+  } = resumeFile;
   console.log('first', resumeFile);
 
   return (
     <div className={`${styles['container']}`}>
       <section className={styles['logo']}>
-        <span className={styles['title']}>{resumeFile.name}</span>
-        <span className={styles['subtitle']}>Full Stack Developer</span>
+        <span className={styles['title']}>{name}</span>
+        <span className={styles['subtitle']}>{title}</span>
       </section>
 
       <aside className={styles['contacts']}>
-        <Link href='mailto:letstalk@bertiltandayamo.me'>
+        <Link href={`mailto:${email}`}>
           <span className='hand' title='Send email'>
-            letstalk@bertiltandayamo.me
+            {email}
           </span>
         </Link>
-        <span>Quito, Ecuador</span>
+        <span>{location}</span>
         <div className={styles['social-networks']}>
-          <Link href={`https://www.linkedin.com/in/bertiltandayamo/`}>
-            <a target='_blank' rel='noopener noreferrer'>
-              <img
-                src='/assets/icons/v2/linkedin.svg'
-                alt='linkedin'
-                title='linkedin'
-              />
-            </a>
-          </Link>
-          <img
-            src='/assets/icons/v2/github-sign.svg'
-            alt='github'
-            title='github'
-          />
+          {Object.entries(socialNetwork).map(([socialNetwork, link]) => (
+            <Link href={link}>
+              <a target='_blank' rel='noopener noreferrer'>
+                <img
+                  src={`/assets/icons/v2/${socialNetwork}.svg`}
+                  alt={socialNetwork}
+                  title={socialNetwork}
+                />
+              </a>
+            </Link>
+          ))}
         </div>
       </aside>
 
       <section className={styles['description']}>
         <div className={styles['description__toggle']}>
-          <ToggleComponent available />
+          <ToggleComponent available={isAvailable} />
           <span>Available</span>
         </div>
-        <span className={styles['description__about-me']}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          autem enim, nihil consequatur animi nemo odit quam quas, repellendus
-          alias nobis dolore assumenda nostrum consectetur sit labore facere,
-          sed odio?
-        </span>
+        <span className={styles['description__about-me']}>{description}</span>
       </section>
 
       <aside className={styles['education']}>
         <h2>Education</h2>
-        <div className={styles['education__content']}>
-          <span className={styles['work-experience__title']}>
-            Interface Design: Web & Mobile
-          </span>
-          <span>
-            Belarusian State University of Informatics and Radioelectronics,
-          </span>
-          <span>2011 - 2015</span>
-          <span>Bachelor’s degree</span>
-        </div>
-        {/* 2 */}
-        <div className={styles['education__content']}>
-          <span className={styles['work-experience__title']}>
-            Interface Design: Web & Mobile
-          </span>
-          <span>
-            Belarusian State University of Informatics and Radioelectronics,
-          </span>
-          <span>2011 - 2015</span>
-          <span>Bachelor’s degree</span>
-        </div>
+        {education.map(({ title, institution, date, degree }) => (
+          <div className={styles['education__content']}>
+            <span className={styles['work-experience__title']}>{title}</span>
+            <span>{institution}</span>
+            <span>{`${date[0]} - ${date[1]}`}</span>
+            <span>{degree}</span>
+          </div>
+        ))}
       </aside>
 
       <section className={styles['work-experience']}>
         <h2>Work Experience</h2>
-        <div className={styles['work-experience__content']}>
-          <span className={styles['work-experience__date']}>
-            Jul 2019 - present
-          </span>
-          <span className={styles['work-experience__title']}>
-            Freelancer Designer
-          </span>
-          <p className={styles['work-experience__place']}>
-            <span>J Creative Solutions</span>
-            <span>Netherlands, Worldwide</span>
-          </p>
-          <span className={styles['work-experience__description']}>
-            Created visuals for digital marketing channels such as social media,
-            promo web and online ads. Developed visual language from scratch or
-            following the branding guidelines. Designed presentations for
-            clients and indoor purposes.
-          </span>
-        </div>
-
-        {/* 2 */}
-
-        <div className={styles['work-experience__content']}>
-          <span className={styles['work-experience__date']}>
-            Jul 2019 - present
-          </span>
-          <span className={styles['work-experience__title']}>
-            Freelancer Designer
-          </span>
-          <p className={styles['work-experience__place']}>
-            <span>J Creative Solutions</span>
-            <span>Netherlands, Worldwide</span>
-          </p>
-          <span className={styles['work-experience__description']}>
-            Created visuals for digital marketing channels such as social media,
-            promo web and online ads. Developed visual language from scratch or
-            following the branding guidelines. Designed presentations for
-            clients and indoor purposes.
-          </span>
-        </div>
+        {workExperience.map(
+          ({ date, title, company, location, description }) => (
+            <div className={styles['work-experience__content']}>
+              <span className={styles['work-experience__date']}>
+                {`${date[0]} - ${date[1]}`}
+              </span>
+              <span className={styles['work-experience__title']}>{title}</span>
+              <p className={styles['work-experience__place']}>
+                <span>{company}</span>
+                <span>{location}</span>
+              </p>
+              <span className={styles['work-experience__description']}>
+                {description}
+              </span>
+            </div>
+          )
+        )}
       </section>
 
       <section className={styles['skills']}>
         <h2>Skills</h2>
         <div className={styles['skills__content']}>
-          <div className={styles['skills__items']}>
-            <span className={styles['skills__title']}>
-              Adobe Creative Suite
-            </span>
-            <span className={styles['skills__description']}>
-              Photoshop, Illustrator, InDesign, XD, After Effects
-            </span>
-          </div>
-          {/* 2 */}
-          <div>
-            <span className={styles['skills__title']}>
-              Adobe Creative Suite
-            </span>
-            <span className={styles['skills__description']}>
-              Photoshop, Illustrator, InDesign, XD, After Effects
-            </span>
-          </div>
-          {/* 2 */}
-          <div>
-            <span className={styles['skills__title']}>
-              Adobe Creative Suite
-            </span>
-            <span className={styles['skills__description']}>
-              Photoshop, Illustrator, InDesign, XD, After Effects
-            </span>
-          </div>
-          {/* 2 */}
-          <div>
-            <span className={styles['skills__title']}>
-              Adobe Creative Suite
-            </span>
-            <span className={styles['skills__description']}>
-              Photoshop, Illustrator, InDesign, XD, After Effects
-            </span>
-          </div>
-          {/* 2 */}
-          <div>
-            <span className={styles['skills__title']}>
-              Adobe Creative Suite
-            </span>
-            <span className={styles['skills__description']}>
-              Photoshop, Illustrator, InDesign, XD, After Effects
-            </span>
-          </div>
+          {skills.map(({ name, items }) => (
+            <div className={styles['skills__items']}>
+              <span className={styles['skills__title']}>{name}</span>
+              <span className={styles['skills__description']}>{items}</span>
+            </div>
+          ))}
         </div>
       </section>
     </div>
