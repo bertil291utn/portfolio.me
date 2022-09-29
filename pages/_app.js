7fac67a302727014d1ac07d0489976859511aa35
Bertil Tandayamo <bertilt4@gmail.com>
2022-09-28 19:54:12 -0500

    add theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
 import HeadComponent from '../components/Head.component';
 import NavbarComponent from '../components/Navbar.component';
 import ScriptComponent from '../components/Script.component';
+import { ThemeProvider } from 'next-themes';
 import '../global.css';
 
 export default function MyApp({ Component, pageProps }) {
   const navbarElements = { portfolio: '/', resume: '/resume' };
 
   return (
-    <>
+    <ThemeProvider>
       <HeadComponent />
       <NavbarComponent navbarElements={navbarElements} />
       <Component {...pageProps} />
       <ScriptComponent />
-    </>
+    </ThemeProvider>
   );
 }
