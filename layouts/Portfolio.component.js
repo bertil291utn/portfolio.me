59171ec283dadbec24daec6c81009d2a9f8fde5f
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-07 11:59:29 -0500

    portfolio work in progress label

--- layouts/Portfolio.component.js
+++ layouts/Portfolio.component.js
@@ -1,6 +1,12 @@
 import styles from './Portfolio.module.scss';
 const PortfolioComponent = () => {
-  return <div>portfolio content</div>;
+  return (
+    <div className={styles['portfolio']}>
+      <span className={styles['portfolio__title']}>
+        Portfolio Work In Progress
+      </span>
+    </div>
+  );
 };
 
 export default PortfolioComponent;
