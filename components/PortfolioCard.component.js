879c99ee6193f5f6b2c86a38097983359df553fd
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-07 16:37:25 -0500

    - add react icons - update portfolio component name

---
+++ components/PortfolioCard.component.js
@@ -0,0 +1,23 @@
+import styles from './PortfolioCard.module.scss';
+import { TbWorld } from 'react-icons/tb';
+import { AiFillGithub, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+
+const PortfolioCard = ({ type, description, projectName }) => {
+  return (
+    <div className={styles['card-content']}>
+      <span className={styles['card-content__tech']}>{type}</span>
+      <span className={styles['card-content__project']}>{projectName}</span>
+      <span className={styles['card-content__description']}>{description}</span>
+      <div className={styles['card-content__icons']}>
+        <TbWorld className={styles['icon-size']} title='Online version' />
+        <AiFillGithub
+          className={styles['icon-size']}
+          title='Github repository'
+        />
+        <AiFillStar className={styles['icon-size']} title='Rate this project' />
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioCard;
--- layouts/Portfolio.component.js
+++ layouts/Portfolio.component.js
@@ -1,12 +1,19 @@
-import PortfolioCards from '../components/PortfolioCards.component';
+import PortfolioCard from '../components/PortfolioCard.component';
 import styles from './Portfolio.module.scss';
+
 const PortfolioComponent = () => {
   return (
     <div className={styles['portfolio']}>
-      <PortfolioCards />
-      <PortfolioCards />
-      <PortfolioCards />
-      <PortfolioCards />
+      <PortfolioCard
+        type='javascript'
+        description='this is money description'
+        projectName='money.js'
+      />
+      <PortfolioCard
+        type='next js'
+        description='this is alpaca something description'
+        projectName='alpaca something.js'
+      />
     </div>
   );
 };
