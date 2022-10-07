10a343d8dfdd67fad5cba3fb3ab721a9aa6a1d5b
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-07 13:45:31 -0500

    portfolio label

--- layouts/Portfolio.component.js
+++ layouts/Portfolio.component.js
@@ -2,9 +2,7 @@ import styles from './Portfolio.module.scss';
 const PortfolioComponent = () => {
   return (
     <div className={styles['portfolio']}>
-      <span className={styles['portfolio__title']}>
-        Portfolio Work In Progress
-      </span>
+      <span className={styles['portfolio__title']}>Building In Progress</span>
     </div>
   );
 };
