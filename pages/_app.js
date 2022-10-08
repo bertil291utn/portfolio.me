eae77e1f18fe99ca49fe0443fadbf1e2920960e0
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-07 19:26:35 -0500

    - footer component

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import HeadComponent from '../components/Head.component';
 import NavbarComponent from '../components/Navbar.component';
 import ScriptComponent from '../components/Script.component';
 import { ThemeProvider } from 'next-themes';
+import FooterComponent from '../components/Footer.component';
 import '../global.css';
 
 export default function MyApp({ Component, pageProps }) {
@@ -16,6 +17,7 @@ export default function MyApp({ Component, pageProps }) {
       <HeadComponent />
       <NavbarComponent navbarElements={navbarElements} />
       <Component {...pageProps} />
+      <FooterComponent />
       <ScriptComponent />
     </ThemeProvider>
   );
