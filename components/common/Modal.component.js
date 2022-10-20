3a4cf29a363dbabb52bd8a59dec5cdfd01b91af2
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-19 22:50:25 -0500

    footer section modal

--- components/common/Modal.component.js
+++ components/common/Modal.component.js
@@ -4,13 +4,15 @@ import styles from './Modal.module.scss';
 
 const ModalComponent = ({ show, setShow, title, children }) => {
   const handleClose = () => setShow(false);
+  const modalRef = useRef(null);
   const { resolvedTheme } = useTheme();
   return (
     <div
       className={`${styles['modal']} ${
         show ? styles['modal__show'] : styles['modal__hide']
       }`}
-      onClick={handleClose}
+      ref={modalRef}
+      // onClick={handleClose}
     >
       <div
         className={`${styles['modal-content']} ${
@@ -22,6 +24,14 @@ const ModalComponent = ({ show, setShow, title, children }) => {
         </span>
         {title && <span className={styles['title']}>{title}</span>}
         {children && <span className={styles['children']}>{children}</span>}
+        <div className={styles['footer']}>
+          <button className={styles['primary-button']}>
+            <span>ok</span>
+          </button>
+          <button className={styles['secondary-button']}>
+            <span>cancel</span>
+          </button>
+        </div>
       </div>
     </div>
   );
