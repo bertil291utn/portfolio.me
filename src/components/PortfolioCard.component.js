6333ed16cd9bce5153e2008b9981922f29e4b452
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-20 19:40:12 -0500

    replace on click action instead link to  add conditions on portfolio icons

--- src/components/PortfolioCard.component.js
+++ src/components/PortfolioCard.component.js
@@ -4,6 +4,7 @@ import { AiFillGithub, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { localStorageKeys } from '@keys/localStorage';
 
 const PortfolioCard = ({
   type,
@@ -24,6 +25,22 @@ const PortfolioCard = ({
     return null;
   }
 
+  const openURL = (URL) => () => {
+    if (
+      window.localStorage.getItem(
+        localStorageKeys.isWeb3User
+      ) /*and doesn't have tokens*/
+    ) {
+      alert('claim free tokens');
+      return;
+    }
+    window.open(URL, '_blank');
+  };
+
+  const rateProject = () => {
+    alert('rate project');
+  };
+
   return (
     <div
       className={`${styles['card-content']} ${
@@ -39,25 +56,26 @@ const PortfolioCard = ({
       </div>
       <div className={styles['card-content__icons']}>
         {overview && (
-          <Link href={overview}>
-            <a target='_blank' rel='noopener noreferrer'>
-              <TbWorld className={styles['icon-size']} title='Online version' />
-            </a>
-          </Link>
+          <TbWorld
+            className={styles['icon-size']}
+            title='Online version'
+            onClick={openURL(overview)}
+          />
         )}
 
         {github && (
-          <Link href={github}>
-            <a target='_blank' rel='noopener noreferrer'>
-              <AiFillGithub
-                className={styles['icon-size']}
-                title='Github repository'
-              />
-            </a>
-          </Link>
+          <AiFillGithub
+            className={styles['icon-size']}
+            title='Github repository'
+            onClick={openURL(github)}
+          />
         )}
 
-        <AiFillStar className={styles['icon-size']} title='Rate this project' />
+        <AiFillStar
+          className={styles['icon-size']}
+          title='Rate this project'
+          onClick={rateProject}
+        />
       </div>
     </div>
   );
