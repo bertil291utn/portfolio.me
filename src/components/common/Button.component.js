34d5af5f30122b70fef9eb9388e0acddb6e704c4
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-25 17:15:58 -0500

    add fav button customized section

--- src/components/common/Button.component.js
+++ src/components/common/Button.component.js
@@ -2,15 +2,24 @@ import styles from './Button.module.scss';
 
 const ButtonComponent = ({ buttonType, btnLabel, onClick, leftIcon }) => {
   const LeftIcon = leftIcon;
-  return (
-    <button
-      className={`${styles['button']} ${styles[buttonType]}`}
-      onClick={onClick}
-    >
-      {leftIcon && <LeftIcon className={styles['left-icon']} />}
-      <span>{btnLabel}</span>
-    </button>
-  );
+  if (buttonType !== 'fav-button') {
+    return (
+      <button
+        className={`${styles['button']} ${styles[buttonType]}`}
+        onClick={onClick}
+      >
+        {leftIcon && <LeftIcon className={styles['left-icon']} />}
+        <span>{btnLabel}</span>
+      </button>
+    );
+  }
+  if (buttonType === 'fav-button') {
+    return (
+      <button className={`${styles[buttonType]}`} onClick={onClick}>
+        <LeftIcon className={styles['left-icon']} />
+      </button>
+    );
+  }
 };
 
 export default ButtonComponent;
