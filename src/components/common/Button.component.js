f2ddda89a085a82979455ff9d6cac31ca3c9b755
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-25 16:55:59 -0500

    modify button with icon

--- src/components/common/Button.component.js
+++ src/components/common/Button.component.js
@@ -1,11 +1,13 @@
 import styles from './Button.module.scss';
 
-const ButtonComponent = ({ buttonType, btnLabel, onClick }) => {
+const ButtonComponent = ({ buttonType, btnLabel, onClick, leftIcon }) => {
+  const LeftIcon = leftIcon;
   return (
     <button
       className={`${styles['button']} ${styles[buttonType]}`}
       onClick={onClick}
     >
+      {leftIcon && <LeftIcon className={styles['left-icon']} />}
       <span>{btnLabel}</span>
     </button>
   );
