2f559315010074b4c8b96466c454ad70acbc911d
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-25 16:56:21 -0500

    implement floating button

--- src/layouts/Portfolio.component.js
+++ src/layouts/Portfolio.component.js
@@ -1,21 +1,37 @@
+import ButtonComponent from '@components/common/Button.component';
 import PortfolioCard from '@components/PortfolioCard.component';
 import PortfolioData from '@data/portfolio.json';
 import styles from './Portfolio.module.scss';
+import { BsCoin } from 'react-icons/bs';
 
 const PortfolioComponent = () => {
+  const getTokensAction = () => {
+    console.log('get tokens');
+  };
+
   return (
-    <div className={styles['portfolio']}>
-      {PortfolioData.map((data) => (
-        <PortfolioCard
-          key={`portfolio-card-${data.id}`}
-          type={data.type}
-          description={data.description}
-          projectName={data.projectName}
-          overview={data.overview}
-          github={data.github}
+    <>
+      <div className={styles['portfolio']}>
+        {PortfolioData.map((data) => (
+          <PortfolioCard
+            key={`portfolio-card-${data.id}`}
+            type={data.type}
+            description={data.description}
+            projectName={data.projectName}
+            overview={data.overview}
+            github={data.github}
+          />
+        ))}
+      </div>
+      <div className={styles['float-button']}>
+        <ButtonComponent
+          buttonType='primary'
+          btnLabel={'Claim free tokens'}
+          onClick={getTokensAction}
+          leftIcon={BsCoin}
         />
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
 
