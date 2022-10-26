47b89c7e6e887d6142212f98446263f50479880f
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-25 21:31:43 -0500

    add placeholder labels in token page

--- src/layouts/Tokens.component.js
+++ src/layouts/Tokens.component.js
@@ -1,4 +1,5 @@
 import ButtonComponent from '@components/common/Button.component';
+import { tokenPageLabel } from '@placeholders/tokens.placeholder';
 import styles from './Token.module.scss';
 
 const TokensComponent = () => {
@@ -7,17 +8,16 @@ const TokensComponent = () => {
   };
   return (
     <div className={styles['content']}>
-      <span className={styles['title']}>Claim your free tokens</span>
-      <p className={styles['description']}>
-        With <b>$BATL</b> tokens, you'll able to rank, review repos and get in
-        touch with Bertil 😂. Furthermore with these tokens you're going to
-        participate in our DAO.
-      </p>
+      <span className={styles['title']}>{tokenPageLabel.title}</span>
+      <p
+        className={styles['description']}
+        dangerouslySetInnerHTML={{ __html: tokenPageLabel.description }}
+      ></p>
       <div className={styles['button']}>
         <ButtonComponent
           buttonType='primary'
           // TODO:show connect wallet in case user has no connect wallet
-          btnLabel='gimme my tokens'
+          btnLabel={tokenPageLabel.buttonLabel}
           onClick={getTokensAction}
         />
       </div>
---
+++ src/placeholders/tokens.placeholder.js
@@ -0,0 +1,6 @@
+export const tokenPageLabel = {
+  title: 'Claim your free tokens',
+  description:
+    "With <b>$BATL</b> tokens, you'll able to rank, review repos and get in touch with Bertil 😂. Furthermore with these tokens you're going to participate in our DAO.",
+  buttonLabel: 'gimme my tokens',
+};
