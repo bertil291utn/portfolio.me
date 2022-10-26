fc56bb8c5ad06e98924fb9c83a12d2b3bd224003
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-26 17:57:54 -0500

    move from app to web3 utils

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiConfig } from 'wagmi';
 import { useEffect } from 'react';
 import HeadComponent from '@components/Head.component';
 import NavbarComponent from '@components/Navbar.component';
@@ -10,26 +8,8 @@ import ScriptComponent from '@components/Script.component';
 import FooterComponent from '@components/Footer.component';
 import { navbarElements } from '@placeholders/navbar.placeholders';
 import { ThemeProvider } from 'next-themes';
+import { chainProv, client } from '@utils/web3';
 import '../css/global.scss';
-
-const { chains, provider } = configureChains(
-  [chain.goerli],
-  [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
-    publicProvider(),
-  ]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -37,8 +17,8 @@ export default function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+    <WagmiConfig client={client}>
+      <RainbowKitProvider chains={chainProv}>
         <ThemeProvider>
           <HeadComponent />
           <NavbarComponent navbarElements={navbarElements} />
--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,6 +1,10 @@
 import { ethers } from 'ethers';
 import { ERC20TokenContractAdd } from 'src/config/contratcs';
 import UpgradeableERC20ABI from '@web3/ABI/UpgradeableERC20.json';
+import { getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
 export const getContractFactory = ({ address, ABI, provider }) => {
   return new ethers.Contract(address, ABI, provider);
@@ -13,3 +17,24 @@ export const getTokenFactory = ({ provider }) => {
     provider,
   });
 };
+
+const { chains, provider } = configureChains(
+  [chain.goerli],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+    publicProvider(),
+  ]
+);
+
+export const chainProv = chains;
+
+const { connectors } = getDefaultWallets({
+  appName: 'Bertil portfolio',
+  chains,
+});
+
+export const client = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
