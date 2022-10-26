ebf91ddb300124edc97c635a67aa90d860cf38f1
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-26 10:01:15 -0500

    add libraries

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import styles from './Navbar.module.scss';
 const NavbarComponent = ({ navbarElements }) => {
   const router = useRouter();
-  //TODO: add a new tab for free tokens (play to earn) or something
+  //TODO: hide tokens option if user already has tokens
   return (
     <ul className={styles['navbar']}>
       {Object.entries(navbarElements)?.map(([navbarName, pathName], index) => (
