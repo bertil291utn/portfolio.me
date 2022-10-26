18ff2d9447ad80e311a3b6fccf7d0421070e9182
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-25 21:41:36 -0500

    update description  label

--- src/placeholders/tokens.placeholder.js
+++ src/placeholders/tokens.placeholder.js
@@ -1,6 +1,6 @@
 export const tokenPageLabel = {
   title: 'Claim your free tokens',
   description:
-    "With <b>$BATL</b> tokens, you'll able to rank, review repos and get in touch with Bertil 😂. Furthermore with these tokens you're going to participate in our DAO.",
+    "With <b>$BATL</b> tokens, you'll able to rank, view repos, buy NFTs and get in touch with Bertil 😂. Furthermore with these tokens you're going to participate in our DAO. <i>(Goerli test network)</i>",
   buttonLabel: 'gimme my tokens',
 };
