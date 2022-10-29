688301874df0e446bf797943aa441225dba928dc
Bertil Tandayamo <bertilt4@gmail.com>
2022-10-29 15:44:21 -0500

    - add and implement toast component

--- src/layouts/Tokens.component.js
+++ src/layouts/Tokens.component.js
@@ -8,9 +8,11 @@ import { useAccount, useSigner } from 'wagmi';
 import styles from './Token.module.scss';
 import { useWalletContext } from 'src/context/WalletProvider';
 import { ERC20TokenContractAdd } from 'src/config/contratcs';
+import ToastComponent from '@components/common/Toast.component';
 
 const TokensComponent = () => {
   const [isWalletConnected, setIsWalletConnected] = useState();
+  const [showToast, setShowToast] = useState(true);
   const { data: signer } = useSigner();
   const { address, isConnected } = useAccount();
   const { userCustomTokenBalance } = useWalletContext();
@@ -33,26 +35,36 @@ const TokensComponent = () => {
     }
   };
   return (
-    <div className={styles['content']}>
-      <span className={styles['title']}>{tokenPageLabel.title}</span>
-      <p
-        className={styles['description']}
-        dangerouslySetInnerHTML={{ __html: tokenPageLabel.description }}
-      ></p>
-      <div className={styles['button']}>
-        <div className={styles['user-connected-btn']}>
-          <ConnectButton showBalance={false} />
+    <>
+      <div className={styles['content']}>
+        <span className={styles['title']}>{tokenPageLabel.title}</span>
+        <p
+          className={styles['description']}
+          dangerouslySetInnerHTML={{ __html: tokenPageLabel.description }}
+        ></p>
+        <div className={styles['button']}>
+          <div className={styles['user-connected-btn']}>
+            <ConnectButton showBalance={false} />
+          </div>
+          {isWalletConnected && (
+            <ButtonComponent
+              className={styles['button__content']}
+              buttonType='primary'
+              btnLabel={tokenPageLabel.buttonLabel}
+              onClick={getTokensAction}
+            />
+          )}
         </div>
-        {isWalletConnected && (
-          <ButtonComponent
-            className={styles['button__content']}
-            buttonType='primary'
-            btnLabel={tokenPageLabel.buttonLabel}
-            onClick={getTokensAction}
-          />
-        )}
       </div>
-    </div>
+      <ToastComponent
+        variant={'success'}
+        closeable
+        show={showToast}
+        setShow={setShowToast}
+      >
+        {'this is my message'}
+      </ToastComponent>
+    </>
   );
 };
 
