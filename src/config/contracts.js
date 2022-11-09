e71606d3fa442ca336b3b8ba93b4adcd95b4b39b
Bertil Tandayamo <bertilt4@gmail.com>
2022-11-09 15:05:26 -0500

    staking and claimable contracts

--- src/config/contracts.js
+++ src/config/contracts.js
@@ -1,2 +1,3 @@
 export const ERC20TokenContractAdd = process.env.NEXT_PUBLIC_ERC20_TOKENS;
-export const ClaimableContractAdd = process.env.NEXT_PUBLIC_CLAIMABLE;
+export const ClaimableContractAdd = process.env.NEXT_PUBLIC_CLAIMABLE_ADDRESS;
+export const StakingContractAdd = process.env.NEXT_PUBLIC_STAKING_ADDRESS;
