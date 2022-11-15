62ed823b3192a2d2da69ccc77b58ebaffcd26aa6
Bertil Tandayamo <bertilt4@gmail.com>
2022-11-15 18:43:54 -0500

    - add rate contract - todo comment

--- src/config/contracts.js
+++ src/config/contracts.js
@@ -1,3 +1,4 @@
 export const ERC20TokenContractAdd = process.env.NEXT_PUBLIC_ERC20_TOKENS;
 export const ClaimableContractAdd = process.env.NEXT_PUBLIC_CLAIMABLE_ADDRESS;
 export const StakingContractAdd = process.env.NEXT_PUBLIC_STAKING_ADDRESS;
+//TODO-WIP: deploy anew smart contract when is ready to prod
