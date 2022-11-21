b71028b3955a86b72277056d663055af8147f7b8
Bertil Tandayamo <bertilt4@gmail.com>
2022-11-21 18:18:13 -0500

    fix placeholder typo

--- src/placeholders/tokens.placeholder.js
+++ src/placeholders/tokens.placeholder.js
@@ -1,8 +1,8 @@
 export const tokenPageLabel = {
   title: 'Claim your 100 free tokens',
   description: (tokenSymbol) =>
-    `With <b>$${tokenSymbol}</b> tokens, you'll able to rank, view repos, buy NFTs and get in touch with Bertil 😂. Furthermore with these tokens you're going to participate in our DAO. <i>(Goerli test network)</i>",
-  buttonLabel: 'gimme my tokens`,
+    `With <b>$${tokenSymbol}</b> tokens, you'll able to rank, view repos, buy NFTs and get in touch with Bertil 😂. Furthermore with these tokens you're going to participate in our DAO. <i>(Goerli test network)</i>`,
+  buttonLabel: 'gimme my tokens',
 };
 
 export const tokenModal = {
