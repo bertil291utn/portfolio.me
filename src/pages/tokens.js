d86b2681cdc370646dfcc346c010cf9ba22fca97
Bertil Tandayamo <bertilt4@gmail.com>
2022-11-24 19:17:18 -0500

    - get nft data urls - add config nft url

--- src/pages/tokens.js
+++ src/pages/tokens.js
@@ -1,7 +1,18 @@
 import TokensComponent from '@layouts/Tokens.component';
+import { nftDataURL } from 'src/config/URLs';
 
-const TokensPage = () => {
-  return <TokensComponent />;
+const TokensPage = ({ nfts }) => {
+  return <TokensComponent NFTData={nfts} />;
 };
 
+export async function getStaticProps() {
+  const res = await fetch(nftDataURL);
+  const nfts = await res.json();
+
+  return {
+    props: {
+      nfts,
+    },
+  };
+}
 export default TokensPage;
