2a2a561ee10178ba10f9653cfbf7e00f6befd164
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-03 14:30:16 -0500

    add erc155 supply

--- web3/contracts/NFTToken1155.sol
+++ web3/contracts/NFTToken1155.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract MultipleEdition is ERC1155, Ownable {
+contract MultipleEdition is ERC1155, ERC1155Supply, Ownable {
     string private _uri;
 
     //"https://gateway.pinata.cloud/ipfs/QmaUh13Q6PXUxD6xwS449dfWcXaS9hmL5wZQGVS68iGoT2/";
@@ -33,5 +34,17 @@ contract MultipleEdition is ERC1155, Ownable {
             return "";
         return
             string(abi.encodePacked(_uri, Strings.toString(_tokenId), ".json"));
+    }
+
+    //overrides
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override(ERC1155, ERC1155Supply) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
