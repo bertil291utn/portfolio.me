1db0863b35f52295c34d0fc48a0f6fd70138bcf8
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-03 18:55:54 -0500

    nft multiple & unique

--- src/config/contracts.js
+++ src/config/contracts.js
@@ -2,3 +2,8 @@ export const ERC20TokenContractAdd = process.env.NEXT_PUBLIC_ERC20_TOKENS;
 export const ClaimableContractAdd = process.env.NEXT_PUBLIC_CLAIMABLE_ADDRESS;
 export const StakingContractAdd = process.env.NEXT_PUBLIC_STAKING_ADDRESS;
 export const RateContractAdd = process.env.NEXT_PUBLIC_RATE_ADDRESS;
+export const NFTUniqueContractAdd = process.env.NEXT_PUBLIC_NFT_UNIQUE_ADDRESS;
+export const NFTMultipleContractAdd =
+  process.env.NEXT_PUBLIC_NFT_MULTIPLE_ADDRESS;
+export const NFTMultipleClaimableContractAdd =
+  process.env.NEXT_PUBLIC_NFT_MULTIPLE_CLAIMABLE_ADDRESS;
