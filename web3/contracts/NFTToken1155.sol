6bc0bc5927ac2be4e1865d96341130c6db6ef6d6
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-02 21:19:20 -0500

    token json uri

--- web3/contracts/NFTToken1155.sol
+++ web3/contracts/NFTToken1155.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MultipleEdition is ERC1155, Ownable {
     constructor() ERC1155("") {}
@@ -14,6 +15,14 @@ contract MultipleEdition is ERC1155, Ownable {
     ) public onlyOwner {
         _mintBatch(msg.sender, ids, amounts, "");
         setApprovalForAll(claimableAddress, true);
+    }
+
+    function getUri(uint256 _tokenId) public view returns (string memory) {
+        require(balanceOf(msg.sender, _tokenId) > 0, "NFT does not exist");
+        return
+            string(
+                abi.encodePacked(uri(0), Strings.toString(_tokenId), ".json")
+            );
     }
 
     function setURI(string calldata newuri) public onlyOwner {
