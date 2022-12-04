36f17aaf4e31fe9ddd5f6e723007192368bdc9b4
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-04 18:34:06 -0500

    - transfer tokens when buy tokens

--- web3/contracts/NFTToken1155Claimable.sol
+++ web3/contracts/NFTToken1155Claimable.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 contract MultipleEditionClaimable {
     //TODO: make payable mintUser when auser buys a nft token when erc 1155 is not free anymore
     //take in mind when is free how to disable require (msg.value>0) condition
+    //Send tokens to original erc155 smart contract
+    //example:  _tokenAddress.transferFrom(msg.sender, address(this), price);
+    //params: IERC20 _tokenAddress
 
     function mintUser(uint256 id, IERC1155 tokenAddress, address owner) public {
         tokenAddress.safeTransferFrom(owner, msg.sender, id, 1, "");
--- web3/contracts/NFTToken721.sol
+++ web3/contracts/NFTToken721.sol
@@ -4,15 +4,24 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./IERC20.sol";
 
 contract UniqueEdition is ERC721, ERC721URIStorage {
     constructor() ERC721("BATANUNIQUE", "BATLA") {}
 
-    //todo-wip: make payable safemint when auser buys a nft token
-    function safeMint(uint256 tokenId, string memory uri) public {
+    function safeMint(
+        uint256 tokenId,
+        string memory uri,
+        uint256 price,
+        IERC20 _tokenAddress
+    ) public {
+        require(price > 0, "Token price is less than zero");
+        _tokenAddress.transferFrom(msg.sender, address(this), price);
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, uri);
     }
+
+    // The following functions are overrides required by Solidity.
 
     function _burn(
         uint256 tokenId
