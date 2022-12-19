893ff8e25a2e645cfc80bb74692f32554251b502
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-19 10:31:25 -0500

    - get posts from hashnode by api rest

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,40 @@
 import BlogComponent from '@layouts/Blog.component';
+import { gql } from '@utils/common';
 
 const Blog = () => {
   return (
-    <BlogComponent/>
+    <BlogComponent />
   );
 };
 
+
+
+export async function getStaticProps() {
+
+  const GET_USER_ARTICLES = `
+    query GetUserArticles($page: Int!) {
+        user(username: "btandayamo") {
+            publication {
+                posts(page: $page) {
+                    title
+                    brief
+                    slug
+                }
+            }
+        }
+    }
+`;
+  const response = await gql(GET_USER_ARTICLES, { page: 0 });
+  const { posts } = response.data.user.publication;
+  //get from substack
+  console.log("🚀 ~ file: blog.js:29 ~ getStaticProps ~ posts", posts)
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 export default Blog;
+
+
--- src/utils/common.js
+++ src/utils/common.js
@@ -20,3 +20,19 @@ export const isTokenCheckPassed = ({
   }
   return true;
 };
+
+
+export async function gql(query, variables = {}) {
+  const data = await fetch('https://api.hashnode.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables
+    })
+  });
+
+  return data.json();
+}
