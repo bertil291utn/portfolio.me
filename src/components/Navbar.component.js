485324c5d4bd2516da8b5408c9fc983f521bcd31
Bertil Tandayamo <bertilt4@gmail.com>
2022-12-26 10:04:07 -0500

    - add connect button

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -1,3 +1,4 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './Navbar.module.scss';
@@ -7,32 +8,34 @@ const NavbarComponent = ({ navbarElements }) => {
   const router = useRouter();
 
   return (
-    <ul className={styles['navbar']}>
-      {Object.values(navbarElements)?.map(
-        ({ label, path, icon: Icon }, index) => {
-          return (
-            <li
-              className={`${styles['list']} ${
-                router.pathname == path ? `${styles['active']}` : ''
-              }`}
-              key={`navbar-${index}`}
-            >
-              <Link href={path}>
-                <a
-                >
-                  <span className={`${styles['navbar__content']}`}>
-                    {label}
-                  </span>
-                  <span className={`${styles['navbar__icon']}`}>
-                    <Icon />
-                  </span>
-                </a>
-              </Link>
-            </li>
-          );
-        }
-      )}
-    </ul>
+    <div className={styles['content']}>
+      <ul className={styles['navbar']}>
+        {Object.values(navbarElements)?.map(
+          ({ label, path, icon: Icon }, index) => {
+            return (
+              <li
+                className={`${styles['list']} ${router.pathname == path ? `${styles['active']}` : ''
+                  }`}
+                key={`navbar-${index}`}
+              >
+                <Link href={path}>
+                  <a
+                  >
+                    <span className={`${styles['navbar__content']}`}>
+                      {label}
+                    </span>
+                    <span className={`${styles['navbar__icon']}`}>
+                      <Icon />
+                    </span>
+                  </a>
+                </Link>
+              </li>
+            );
+          }
+        )}
+      </ul>
+      <ConnectButton showBalance={false} />
+    </div>
   );
 };
 
