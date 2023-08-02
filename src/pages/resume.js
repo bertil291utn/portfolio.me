936e2954ef144c0211b2d9104d807eb18b76c1e1
Bertil Tandayamo <bertilt4@gmail.com>
2023-08-02 12:41:36 -0500

    Sort work experience date asc

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,7 +1,24 @@
 import ResumeComponent from '@layouts/Resume.component';
 import { getResume } from '@utils/firebaseFunctions';
 
+const extractYear = (dateString) => {
+  const [_, year] = dateString.split(' ');
+  return year;
+};
+
+
+const sortWorkExperienceDate = (resumeData) => {
+  resumeData.workExperience.sort((a, b) => {
+    const yearA = parseInt(extractYear(a.date[0]), 10);
+    const yearB = parseInt(extractYear(b.date[0]), 10);
+
+    return yearB - yearA;
+  });
+}
+
+
 const ResumePage = ({ resume }) => {
+  sortWorkExperienceDate(resume);
   return <ResumeComponent resumeData={resume} />;
 };
 
